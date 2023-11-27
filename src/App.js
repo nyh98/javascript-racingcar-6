d9f90faa0af4a97474ff84958ad5a61f5768bd40
nyh98 <nsi3358@naver.com>
2023-11-27 20:23:09 +0900

    2주차 복습 시작

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
-import Input from './racingcar/domain/Input.js';
-import GameStart from './racingcar/domain/GameStart.js';
-
 class App {
-  async play() {
-    //게임 시작
-    const CAR_NAMES = await Input.uswerInputCarName();
-    const CYCLE_SIZE = await Input.uswerInputCycle();
-
-    for (let i = 0; i < CYCLE_SIZE; i++) {
-      GameStart.carMoveTryResult(CAR_NAMES);
-      GameStart.carMoveDecide(CAR_NAMES);
-    }
-  }
+  async play() {}
 }
 export default App;
